87052b02c2435a77ea695183534dddea56263625
Paolo Bertinetti <bertinetti@sirius-ea.com>
2025-07-03 09:42:04 +0200

    add summary to model

--- VireoXiniParser/Model/IniDocument.cs
+++ VireoXiniParser/Model/IniDocument.cs
@@ -1,5 +1,14 @@
 ﻿namespace VireoXiniParser.Model;
 
+/// <summary>
+/// Represents an INI document consisting of a collection of sections.
+/// </summary>
+/// <param name="Sections">The list of sections contained in the INI document.</param>
 public record IniDocument(IReadOnlyList<IniSection> Sections);
 
+/// <summary>
+/// Represents a section within an INI document, containing a name and a set of key-value pairs.
+/// </summary>
+/// <param name="Name">The name of the section.</param>
+/// <param name="KeyValues">The key-value pairs within the section.</param>
 public record IniSection(string Name, IReadOnlyDictionary<string, string> KeyValues);
